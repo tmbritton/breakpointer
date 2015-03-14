1137ad9f12ac2e3cb4c81d4ed5ddca67f1aaef15
Thomas Britton <britton.tm@gmail.com>
2015-03-13 23:41:51 -0500

    Now emitting events

--- breakpointer.js
+++ breakpointer.js
@@ -5,16 +5,50 @@ define('breakpointer', function(){
     var bp = {
       breakpoints: [],
       options: {},
+      lastbreakpoint: null,
 
-      createEvents: function() {
-        console.log(bp.breakpoints);
-        bp.breakpoints.forEach(function(breakpoint){
-          var enterEvent = document.createEvent('CustomEvent'),
-              exitEvent = document.createEvent('CustomEvent');
+      addEvent: function(elem, type, callback) {
+        if (elem == null || typeof(elem) == 'undefined') return;
+        if (elem.addEventListener) {
+          elem.addEventListener(type, callback, false);
+        } 
+        else if ( elem.attachEvent ) {
+          elem.attachEvent("on" + type, callback);
+        } 
+        else {
+          elem["on"+type]=callback;
+        }
+      },
 
-          enterEvent.initEvent('enterbreakpoint' + breakpoint, true, true);
-          exitEvent.initEvent('exitbreakpoint' + breakpoint, true, true);
+      emitEvent: function(type, breakpoint) {
+        var event = document.createEvent('Event');
+        event.initEvent(type + breakpoint);
+        document.dispatchEvent(event);
+      },
+
+      findWhichBreakpoint: function(width) {
+        var previousBreakpoint = null,
+            found = false,
+            currentBreakpoint = '';
+        bp.breakpoints.filter(function(value, index){
+          if (width >= previousBreakpoint && width < value && found == false) {
+            found = true;
+            return true;
+          }
+          if (!found) { 
+            previousBreakpoint = value;
+          }
         });
+        return previousBreakpoint;
+      },
+
+      getWindowWidth: function() {
+        var w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth;
+        return x;
       },
 
       /**
@@ -27,7 +61,19 @@ define('breakpointer', function(){
           breakpoints = [320, 480, 640, 720, 960, 1024, 1280];
         }
         bp.breakpoints = breakpoints;
-        bp.createEvents();
+        bp.addEvent(window, 'resize', bp.resizeCallback);
+      },
+
+      resizeCallback: function() {
+        var lastsize = bp.lastsize,
+            width = bp.getWindowWidth(),
+            breakpoint = bp.findWhichBreakpoint(width);
+        
+        if (bp.lastbreakpoint !== breakpoint) {
+          bp.emitEvent('exit', bp.lastbreakpoint);
+          bp.emitEvent('enter', breakpoint);
+          bp.lastbreakpoint = breakpoint;
+        }
       },
     };
 
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 require(['breakpointer'], function(breakpointer) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-    var bp = breakpointer();
-    bp.init([320, 480, 640, 720, 960, 1024, 1280, 1400, 1600]);
+
+  var bp = breakpointer(),
+      breakpoints = [320, 480, 640, 720, 960, 1024, 1280, 1400, 1600];
+  bp.init(breakpoints);
+
+  breakpoints.forEach(function(bp){
+    document.addEventListener('enter' + bp, function(){
+      console.log('Entered ' + bp + 'px breakpoint.');
+    });
+    document.addEventListener('exit' + bp, function(){
+      console.log('Exited ' + bp + 'px breakpoint.');
+    });
+  });
 });
